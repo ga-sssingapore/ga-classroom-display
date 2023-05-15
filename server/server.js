f8d48caa1cc51defd651ba2f7125b5bafc11bb68
ga-sssingapore <129036928+ga-sssingapore@users.noreply.github.com>
2023-05-15 09:24:37 +0800

    fix missing )

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,PUT,POST,DELETE,PATCH");
   res.header("Access-Control-Allow-Headers", "Origin,X-Requested-With,Content-Type,Accept,Authorization");
   next()
-}
+});
 
 app.use(morgan("dev"));
 app.use("/auth", authRoutes);
